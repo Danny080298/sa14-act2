5d819b6d0581dbc6adeedd85db8cd6a4dff8a9fa
Danny Nguyen <dqngyen1@memphis.edu>
2024-04-03 18:39:11 -0500

    Completed Animated page

--- animation-task/app.js
+++ animation-task/app.js
@@ -1,42 +1,23 @@
-
-var right = document.getElementById('control-right');
-var down = document.getElementById('control-down');
-var up = document.getElementById('control-up');
-var left = document.getElementById('control-left');
-var div = document.getElementById('movable-div');
-var toggle = false;
-
-right.onclick = function() {
-    toggle = !toggle;
-    if (toggle) {
+document.addEventListener('DOMContentLoaded', function() {
+    var div = document.getElementById('movable-div');
+    
+    document.getElementById('control-right').onclick = function() {
         div.style.transform = 'translateX(300px)';
-    } else {
-        div.style.transform = 'translateX(0px)';
-    }
-};
-down.onclick = function() {
-    toggle = !toggle;
-    if (toggle) {
-        div.style.transform = 'translateY(300px)';
-    } else {
-        div.style.transform = 'translateY(600px)';
-    }
-};
-up.onclick = function() {
-    toggle = !toggle;
-    if (toggle) {
-        div.style.transform = 'translateY(600px)';
-    } else {
-        div.style.transform = 'translateY(300px)';
-    }
-};
-left.onclick = function() {
-    toggle = !toggle;
-    if (toggle) {
-        div.style.transform = 'translateY(600px)';
-    } else {
-        div.style.transform = 'translateY(300px)';
-    }
-};
+    };
 
+    document.getElementById('control-down').onclick = function() {
+        div.style.transform = 'translateY(300px)';
+    };
 
+    document.getElementById('control-up').onclick = function() {
+        div.style.transform = 'translateY(-300px)';
+    };
+
+    document.getElementById('control-left').onclick = function() {
+        div.style.transform = 'translateX(-300px)';
+    };
+
+    document.getElementById('control-reset').onclick = function() {
+        div.style.transform = 'translate(0, 0)';
+    };
+});
