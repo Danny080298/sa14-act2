bdd2f75db94e8d3115c481d595abd1ecb9076222
Danny Nguyen <dqngyen1@memphis.edu>
2024-04-03 17:21:21 -0500

    Completed website with css styling and edit button

--- todo/app.js
+++ todo/app.js
@@ -16,18 +16,70 @@ function addTask() {
         <button class="delete-btn">Delete</button>
     `;
     taskList.appendChild(li);
-
-  
     li.querySelector('.edit-btn').addEventListener('click', function() {
         editTask(this.parentElement);
     });
-    
     li.querySelector('.delete-btn').addEventListener('click', function() {
         deleteTask(this.parentElement);
     });
 
     document.getElementById('task-title').value = '';
     document.getElementById('task-details').value = '';
+}
+function editTask(taskItem) {
+    
+    const titleSpan = taskItem.querySelector('.task-title');
+    const detailsSpan = taskItem.querySelector('.task-details');
+    
+    const currentTitle = titleSpan.innerText;
+    const currentDetails = detailsSpan.innerText;
+
+    
+    const titleInput = document.createElement('input');
+    titleInput.type = 'text';
+    titleInput.value = currentTitle;
+    titleInput.className = 'edit-title';
+
+    const detailsInput = document.createElement('textarea');
+    detailsInput.value = currentDetails;
+    detailsInput.className = 'edit-details';
+
+  
+    taskItem.replaceChild(titleInput, titleSpan);
+    taskItem.replaceChild(detailsInput, detailsSpan);
+
+    const saveChanges = () => saveTask(taskItem, titleInput, detailsInput);
+    const editButton = taskItem.querySelector('.edit-btn');
+    editButton.innerText = 'Save';
+    editButton.onclick = saveChanges; 
+    
+}
+function saveTask(taskItem, titleInput, detailsInput) {
+    const newTitle = titleInput.value;
+    const newDetails = detailsInput.value;
+
+    const titleSpan = document.createElement('span');
+    titleSpan.className = 'task-title';
+    titleSpan.innerText = newTitle;
+
+    const detailsSpan = document.createElement('span');
+    detailsSpan.className = 'task-details';
+    detailsSpan.innerText = newDetails;
+
+    taskItem.replaceChild(titleSpan, titleInput);
+    taskItem.replaceChild(detailsSpan, detailsInput);
+
+    const saveButton = taskItem.querySelector('.edit-btn');
+    saveButton.innerText = 'Edit';
+    saveButton.removeEventListener('click', function() {
+        saveTask(taskItem, titleInput, detailsInput);
+    });
+    saveButton.addEventListener('click', function() {
+        editTask(taskItem);
+    });
+    const editButton = taskItem.querySelector('.edit-btn');
+    editButton.innerText = 'Edit';
+    editButton.onclick = () => editTask(taskItem);
 }
 
 
