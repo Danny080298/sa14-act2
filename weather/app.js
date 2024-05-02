cb8ba0dd41605b89669dfea0f2fe0696ee2d3dc1
Danny Nguyen <dqngyen1@memphis.edu>
2024-05-02 13:24:25 -0500

    Fix weather page to display weather icon

--- weather/app.js
+++ weather/app.js
@@ -13,28 +13,38 @@ async function fetchWeather(city) {
 }
 
 function displayCurrentWeather(data) {
+    
     const currentDetails = document.getElementById('current-details');
+    const iconUrl = data.current.condition.icon.startsWith('http') ? data.current.condition.icon : `https://${data.current.condition.icon}`;
+
     currentDetails.innerHTML = `
         
+
         <p>Time: ${new Date().toLocaleTimeString()}</p>
         <p>Temperature: ${data.current.temp_c}°C</p>
-        <img src="${data.current.condition.icon}">
+        <img src="${iconUrl}" alt="Weather Icon">
+
         <p>Condition: ${data.current.condition.text}</p>
         <p>Humidity: ${data.current.humidity} %</p>
+        
+        
     `;
 }
 
 
 function displayForecast(forecast) {
     const forecastDetails = document.getElementById('forecast-details');
-    forecastDetails.innerHTML = forecast.map(day => `
-        <div>
-            <h3>${day.date}</h3>
-            <img src="${day.day.condition.icon}">
-            <p>Tempurate: ${day.day.avgtemp_c}°C</p>
-            <p>Condition: ${day.day.condition.text}</p>
-            <p>Humidity: ${day.day.avghumidity} %</p>
-            <p> ---------------------- </p>
-        </div>
-    `).join('');
+    forecastDetails.innerHTML = forecast.map(day => {
+        const iconUrl = day.day.condition.icon.startsWith('http') ? day.day.condition.icon : `https://${day.day.condition.icon}`;
+        return `
+            <div>
+                <h3>${day.date}</h3>
+                <img src="${iconUrl}" alt="Weather Icon">
+                <p>Temperature: ${day.day.avgtemp_c}°C</p>
+                <p>Condition: ${day.day.condition.text}</p>
+                <p>Humidity: ${day.day.avghumidity} %</p>
+                <p> ---------------------- </p>
+            </div>
+        `;
+    }).join('');
 }
